f556383c885359f69d23348553146d2f92cc4f8a
Don Cooper <don_cooper@hotmail.com>
2017-08-31 10:31:35 -0400

    Initial commit of previous work on a React.js exercise

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './Calculator';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Calculator />,
+  document.getElementById('root')
+ );
